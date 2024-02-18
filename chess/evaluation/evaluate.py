f0b96df58aa940c9a99df98da5b2273db55211bb
Porcupinee <htolley0@gmail.com>
2024-02-18 23:50:31 +1000

    Implemented pawn shield scoring + returning accurate eval values from func

--- chess/evaluation/evaluate.py
+++ chess/evaluation/evaluate.py
@@ -1,7 +1,9 @@
 import chess
 from piece_square_tables import PieceSquareTables
+from precomputed_evaluation_data import PrecomputedEvaluationData
 
-class Evaluation():
+
+class Evaluation:
     PAWN_VALUE = 100
     KNIGHT_VALUE = 300
     BISHOP_VALUE = 320
@@ -31,24 +33,47 @@ class Evaluation():
         # calculate material
         whiteMaterialInfo = self.get_material_info(chess.WHITE)
         blackMaterialInfo = self.get_material_info(chess.BLACK)
-        
+
         # plain material score advantage
         self.whiteEval.material_score = whiteMaterialInfo.material_score
         self.blackEval.material_score = blackMaterialInfo.material_score
-        
+
         # score based on position of pieces
-        self.whiteEval.piece_square_score = self.evaluate_piece_square_tables(chess.WHITE, blackMaterialInfo.endgameT)
-        self.blackEval.piece_square_score = self.evaluate_piece_square_tables(chess.BLACK, whiteMaterialInfo.endgameT)
+        self.whiteEval.piece_square_score = self.evaluate_piece_square_tables(
+            chess.WHITE, blackMaterialInfo.endgameT
+        )
+        self.blackEval.piece_square_score = self.evaluate_piece_square_tables(
+            chess.BLACK, whiteMaterialInfo.endgameT
+        )
 
         # mop up score for both colors
         # encouraging the engine to centralise kings in winning endgames
-        self.whiteEval.mopUpScore = self.mop_up_eval(chess.WHITE, whiteMaterialInfo, blackMaterialInfo)
-        self.blackEval.mopUpScore = self.mop_up_eval(chess.BLACK, blackMaterialInfo, whiteMaterialInfo)
+        self.whiteEval.mopUpScore = self.mop_up_eval(
+            chess.WHITE, whiteMaterialInfo, blackMaterialInfo
+        )
+        self.blackEval.mopUpScore = self.mop_up_eval(
+            chess.BLACK, blackMaterialInfo, whiteMaterialInfo
+        )
 
         # evaluate pawn structure for both colors
         self.whiteEval.pawn_score = self.evaluate_pawns(chess.WHITE)
         self.blackEval.pawn_score = self.evaluate_pawns(chess.BLACK)
 
+        # evaluate king pawn shield for both colors
+        self.whiteEval.pawn_shield_score = self.evaluate_king_pawn_shield(
+            chess.WHITE, blackMaterialInfo, self.blackEval.piece_square_score
+        )
+        self.blackEval.pawn_shield_score = self.evaluate_king_pawn_shield(
+            chess.BLACK, whiteMaterialInfo, self.whiteEval.piece_square_score
+        )
+
+        if board.turn == chess.WHITE:
+            perspective = 1
+        else:
+            perspective = -1
+
+        evalSum = self.whiteEval.sum() - self.blackEval.sum()
+        return evalSum * perspective
 
     def get_material_info(self, color):
         """
@@ -77,18 +102,40 @@ class Evaluation():
         colourIndex = 0 if color == chess.WHITE else 6
 
         # add the piece square table for the pieces for the given color
-        value += self.evaluate_piece_square_table(self.piece_square_tables.tables[colourIndex + chess.KNIGHT], color, chess.KNIGHT)
-        value += self.evaluate_piece_square_table(self.piece_square_tables.tables[colourIndex + chess.BISHOP], color, chess.BISHOP)
-        value += self.evaluate_piece_square_table(self.piece_square_tables.tables[colourIndex + chess.ROOK], color, chess.ROOK)
-        value += self.evaluate_piece_square_table(self.piece_square_tables.tables[colourIndex + chess.QUEEN], color, chess.QUEEN)
+        value += self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[colourIndex + chess.KNIGHT],
+            color,
+            chess.KNIGHT,
+        )
+        value += self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[colourIndex + chess.BISHOP],
+            color,
+            chess.BISHOP,
+        )
+        value += self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[colourIndex + chess.ROOK], color, chess.ROOK
+        )
+        value += self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[colourIndex + chess.QUEEN],
+            color,
+            chess.QUEEN,
+        )
 
-        # interpolate between early and late game 
-        pawnEarlyGame = self.evaluate_piece_square_table(self.piece_square_tables.tables[colourIndex + chess.PAWN], color, chess.PAWN)
-        pawnsLateGame = self.evaluate_piece_square_table(self.piece_square_tables.tables[13 + (not color)], color, chess.PAWN)
+        # interpolate between early and late game
+        pawnEarlyGame = self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[colourIndex + chess.PAWN], color, chess.PAWN
+        )
+        pawnsLateGame = self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[13 + (not color)], color, chess.PAWN
+        )
         value += pawnEarlyGame * (1 - endgameT) + pawnsLateGame * endgameT
 
-        kingEarlyGame = self.evaluate_piece_square_table(self.piece_square_tables.tables[colourIndex + chess.KING], color, chess.KING)
-        kingLateGame = self.evaluate_piece_square_table(self.piece_square_tables.tables[15 + (not color)], color, chess.KING)
+        kingEarlyGame = self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[colourIndex + chess.KING], color, chess.KING
+        )
+        kingLateGame = self.evaluate_piece_square_table(
+            self.piece_square_tables.tables[15 + (not color)], color, chess.KING
+        )
         value += kingEarlyGame * (1 - endgameT) + kingLateGame * endgameT
 
         return value
@@ -98,7 +145,7 @@ class Evaluation():
         for square in self.get_board().pieces(piece, color):
             value += self.piece_square_tables.read(table, color, square)
         return value
-    
+
     def edge_distance(self, square):
         """
         Returns the distance from a square to the edge of the board
@@ -106,11 +153,19 @@ class Evaluation():
         rank, file = divmod(square, 8)
         return min(rank, 7 - rank, file, 7 - file)
 
-    def mop_up_eval(self, color, myMaterial: 'Evaluation.MaterialInfo', enemyMaterial: 'Evaluation.MaterialInfo'):
+    def mop_up_eval(
+        self,
+        color,
+        myMaterial: "Evaluation.MaterialInfo",
+        enemyMaterial: "Evaluation.MaterialInfo",
+    ):
         """
         Returns the mop up score for the given color
         """
-        if (myMaterial.material_score > enemyMaterial.material_score + self.PAWN_VALUE * 2) and enemyMaterial.endgameT > 0:
+        if (
+            myMaterial.material_score
+            > enemyMaterial.material_score + self.PAWN_VALUE * 2
+        ) and enemyMaterial.endgameT > 0:
             mopUpScore = 0
 
             if color == chess.WHITE:
@@ -119,10 +174,14 @@ class Evaluation():
             else:
                 friendlyKingSquare = self.get_board().king(chess.BLACK)
                 enemyKingSquare = self.get_board().king(chess.WHITE)
-            
-            mopUpScore += (14 - chess.square_distance(friendlyKingSquare, enemyKingSquare)) * 4
 
-            mopUpScore += chess.square_manhattan_distance(chess.D4, enemyKingSquare) * 10
+            mopUpScore += (
+                14 - chess.square_distance(friendlyKingSquare, enemyKingSquare)
+            ) * 4
+
+            mopUpScore += (
+                chess.square_manhattan_distance(chess.D4, enemyKingSquare) * 10
+            )
             return mopUpScore * enemyMaterial.endgameT
         return 0
 
@@ -139,18 +198,20 @@ class Evaluation():
             if self.is_passed_pawn(pawn_square, color):
                 # passed pawn bonus
                 if color == chess.WHITE:
-                    value += self.passed_pawn_bonuses[7 - chess.square_rank(pawn_square)]
+                    value += self.passed_pawn_bonuses[
+                        7 - chess.square_rank(pawn_square)
+                    ]
                 else:
                     value += self.passed_pawn_bonuses[chess.square_rank(pawn_square)]
-            
+
             if self.is_isolated_pawn(pawn_square, color):
                 # isolated pawn penalty
                 num_isolated_pawns += 1
-        
+
         value += self.isolated_pawn_penalty_by_count[num_isolated_pawns]
 
         return value
-    
+
     def is_passed_pawn(self, square, color):
         """
         Returns True if the pawn on the given square is a passed pawn for the given color
@@ -163,7 +224,11 @@ class Evaluation():
         passed = True
 
         end_rank = 7 if color == chess.WHITE else 0
-        rank_range = range(pawn_rank + 1, end_rank) if color == chess.WHITE else range(pawn_rank - 1, end_rank, -1)
+        rank_range = (
+            range(pawn_rank + 1, end_rank)
+            if color == chess.WHITE
+            else range(pawn_rank - 1, end_rank, -1)
+        )
 
         # loop through ranks and files to determine if opposing pawn is blocking
         for rank in rank_range:
@@ -171,12 +236,14 @@ class Evaluation():
                 if file >= 0 and file <= 7:
 
                     target_sqare = chess.square(file, rank)
-                    if self.get_board().piece_at(target_sqare) == chess.Piece(chess.PAWN, opposing_color):
+                    if self.get_board().piece_at(target_sqare) == chess.Piece(
+                        chess.PAWN, opposing_color
+                    ):
                         passed = False
                         break
             if not passed:
                 break
-        
+
         return passed
 
     def is_isolated_pawn(self, square, color):
@@ -190,13 +257,91 @@ class Evaluation():
 
         for file in [pawn_file - 1, pawn_file + 1]:
             if file >= 0 and file <= 7:
-                adjacent_pawn = self.get_board().pieces(chess.PAWN, color) & chess.SquareSet(chess.BB_FILES[file])
+                adjacent_pawn = self.get_board().pieces(
+                    chess.PAWN, color
+                ) & chess.SquareSet(chess.BB_FILES[file])
 
                 if adjacent_pawn:
                     isolated = False
                     break
         return isolated
 
+    def evaluate_king_pawn_shield(
+        self, color, enemy_material: "Evaluation.MaterialInfo", enemy_piece_square_score
+    ):
+        """
+        Returns the king pawn shield score for the given color
+        """
+        if enemy_material.endgameT >= 1:
+            return 0
+
+        penalty = 0
+        uncastled_king_penalty = 0
+        is_white = color == chess.WHITE
+        king_square = self.get_board().king(color)
+        friendly_pawn = chess.Piece(chess.PAWN, color)
+
+        # Check if the king is present on the board - purely for testing purposes
+        if king_square is None:
+            return 0
+
+        king_file = chess.square_file(king_square)
+        king_rank = chess.square_rank(king_square)
+
+        if king_file <= 2 or king_file >= 5:
+            squares = (
+                self.piece_square_tables.PawnShieldSquaresWhite[king_square]
+                if is_white
+                else self.piece_square_tables.PawnShieldSquaresBlack[king_square]
+            )
+
+            for i, shield_square in enumerate(shield_squares // 2):
+
+                if self.get_board().piece_at(shield_square) != friendly_pawn:
+
+                    if len(shield_squares) > 3 and self.get_board().piece_at(
+                        shield_squares[i + 3] == friendly_pawn
+                    ):
+                        penalty += king_pawn_shield_scores[i + 3]
+
+                else:
+                    penalty += king_pawn_shield_scores[i]
+
+            penalty *= penalty
+
+        else:
+            enemy_development_score = max(
+                0, min((enemy_piece_square_score + 10) / 130, 1)
+            )
+            uncastled_king_penalty = 50 * enemy_development_score
+
+        open_file_against_king_penalty = 0
+
+        if enemy_material.num_rooks > 1 or (
+            enemy_material.num_rooks > 0 and enemy_material.num_queens > 0
+        ):
+
+            clamped_king_file = max(1, min(king_file, 6))
+            my_pawns = enemy_material.enemy_pawns
+
+            for attack_file in range(clamped_king_file - 1, clamped_king_file + 2):
+                if color == chess.WHITE:
+                    if all(
+                        not self.get_board().piece_at(chess.square(attack_file, rank))
+                        for rank in range(king_rank, 8)
+                    ):
+                        if attack_file == king_file:
+                            open_file_against_king_penalty += 25
+                        else:
+                            open_file_against_king_penalty += 15
+
+        pawn_shield_weight = 1 - enemy_material.endgameT
+        if len(self.get_board().pieces(chess.QUEEN, not color)) == 0:  # no enemy queens
+            pawn_shield_weight *= 0.6
+
+        return (
+            -penalty - uncastled_king_penalty - open_file_against_king_penalty
+        ) * pawn_shield_weight
 
     class EvaluationData:
         def __init__(self):
@@ -262,4 +407,3 @@ class Evaluation():
             )
 
             self.endgameT = 1 - min(1, endgame_weight_sum / float(endgame_start_weight))
-
