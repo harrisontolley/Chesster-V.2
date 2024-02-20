840af3f55360844024ad020c80b4dc1b8ebc2350
Porcupinee <htolley0@gmail.com>
2024-02-20 22:19:11 +1000

    Minor changes to minimax to complete more accurate moves

--- core/search/search.py
+++ core/search/search.py
@@ -1,43 +1,34 @@
 import chess
 import time
-
+import random
 from ..evaluation.evaluate import Evaluation
 
 
 class Search:
+    posInf = 999999999999
+    negInf = -posInf
+
     def __init__(self):
         self.eval = Evaluation()
-
-    def basic_search(self, board):
-        """
-        Finds the best move for the given color on 1 ply.
-        """
-        best_move = None
-        best_score = -9999
-
-        for move in board.legal_moves:
-            board.push(move)
-            score = -self.eval.evaluate(board)
-            board.pop()
-
-            if score > best_score:
-                best_score = score
-                best_move = move
-
-        return best_move
+        self.board = None
 
     def minimax(
         self, board, depth, is_maximizing, alpha=-float("inf"), beta=float("inf")
     ):
-        if board.is_game_over():
-            # Assign checkmate and stalemate scores
-            if board.is_checkmate():
-                return (-float("inf"), None) if is_maximizing else (float("inf"), None)
-            else:
-                return (0, None)  # Draw or stalemate
+        if board.is_checkmate():
+            return (
+                (self.negInf + depth, None)
+                if is_maximizing
+                else (self.posInf - depth, None)
+            )
+        elif board.is_stalemate() or board.is_insufficient_material() or depth == 0:
+            return (self.eval.evaluate(board), None)
 
-        if depth == 0 or board.is_game_over():
-            return self.eval.evaluate(board), None
+        self.board = board
+
+        legal_moves = list(board.legal_moves)
+        if len(legal_moves) == 1:
+            return (0, legal_moves[0])
 
         if is_maximizing:
             max_eval = -float("inf")
@@ -46,13 +37,16 @@ class Search:
                 board.push(move)
                 eval, _ = self.minimax(board, depth - 1, False, alpha, beta)
                 board.pop()
+
                 if eval > max_eval:
                     max_eval = eval
                     best_move = move
+
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
-            return max_eval, best_move
+
+            return (max_eval, best_move)
         else:
             min_eval = float("inf")
             best_move = None
@@ -60,10 +54,13 @@ class Search:
                 board.push(move)
                 eval, _ = self.minimax(board, depth - 1, True, alpha, beta)
                 board.pop()
+
                 if eval < min_eval:
                     min_eval = eval
                     best_move = move
+
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
-            return min_eval, best_move
+
+            return (min_eval, best_move)
